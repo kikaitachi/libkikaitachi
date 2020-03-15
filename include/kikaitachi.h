7467d584c0a065bf5a87c1e81d138912ce35ae96
Mykolas Juraitis <mykolas.juraitis@sg-tech.co.uk>
2020-03-15 16:33:59 +0000

    Start implementing sending messages

--- include/kikaitachi.h
+++ include/kikaitachi.h
@@ -1,7 +1,11 @@
 #ifndef _KIKAITACHI_
 #define _KIKAITACHI_
 
+#define KT_MESSAGE_TELEMETRY_ITEM 0
+
 enum KT_TELEMETRY_TYPE {
+	KT_TELEMETRY_TYPE_GROUP,
+	KT_TELEMETRY_TYPE_ACTION,
 	KT_TELEMETRY_TYPE_STRING,
 	KT_TELEMETRY_TYPE_DOUBLE
 };
@@ -20,6 +24,8 @@ typedef struct kt_telemetry_item {
 kt_telemetry_item *kt_telemetry_create_item(int id, int name_len, char* name, enum KT_TELEMETRY_TYPE type, int value_len, char* value);
 
 void kt_telemetry_free_item(kt_telemetry_item *item);
+
+int kt_telemetry_send(int fd, kt_telemetry_item *item);
 
 #endif
 
